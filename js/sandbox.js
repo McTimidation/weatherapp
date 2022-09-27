e779818bf95e5e43c8ffe1bdcfb69a11d39e428a
McTimidation <michael.mcgraw@student.awesomeinc.org>
2022-09-27 20:56:19 +0000

    laying out elements

---
+++ js/sandbox.js
@@ -0,0 +1,4 @@
+// let header = document.createElement('div').className('header container');
+// let headerText = document.createElement('h1').className('text-center');
+// let mainBtn = document.createElement('button').setAttribute('id','mainBtn');
+// let zipInput = document.createElement('input').setAttribute('type','text');
